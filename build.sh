07bdb51fc523c36365123536f64145f7525f2309
Raka Prasetya Pratama <123168129+hiratazx@users.noreply.github.com>
2024-11-24 11:32:10 +0800

    Building: RisingOS for Galaxy S10e (beyond0lte)

--- build.sh
+++ build.sh
@@ -18,53 +18,21 @@ mkdir -p ~/.config/rclone
 echo "$rclone_config" > ~/.config/rclone/rclone.conf
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo "$id_rsa" > ~/.ssh/id_rsa
-echo "$id_rsa_pub" > ~/.ssh/id_rsa.pub
-chmod 400 ~/.ssh/id_rsa
 git config --global user.email "$user_email"
 git config --global user.name "$user_name"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-echo "$known_hosts" > ~/.ssh/known_hosts
-echo "$user_credentials" > ~/.git-credentials && git config --global credential.helper store
-
-tg_sendText "Downloading ccache"
-cd /tmp
-rclone copy aosp:corvus_ccache.tar.gz /tmp/
-tar xf corvus_ccache.tar.gz
-rm -f corvus_ccache.tar.gz
 
 tg_sendText "Syncing rom"
 mkdir -p /tmp/rom
 cd /tmp/rom
-repo init --no-repo-verify --depth=1 -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --no-repo-verify --depth=1 -u https://github.com/RisingTechOSS/android -b fourteen -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/hiratazx/local_manifest --depth=1 .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j6 || repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
-
-tg_sendText "Downloading trees"
-git clone https://github.com/Gabriel260/android_hardware_samsung-2 hardware/samsung
-git clone https://github.com/geckyn/android_kernel_samsung_exynos7885 kernel/samsung/exynos7885 --depth=1
-git clone https://github.com/Gabriel260/android_device_samsung_a10-common -b corvus device/samsung
-git clone https://github.com/Gabriel260/proprietary_vendor_samsung_a10-common vendor/samsung
-
-# Prebuilt kernel patch
-tg_sendText "Applying Patches"
-cd vendor/corvus
-git remote add k https://github.com/crdroidandroid/android_vendor_crdroid
-git fetch k
-git cherry-pick ef2ec82665c547bd9e6b05a45dbb2cc4fc1b06b4
-cd -
-cd frameworks/base/data/etc
-rm -f com.android.systemui.xml
-git clone https://github.com/Gabriel260/temp
-mv temp/privapp_whitelist_com.android.systemui.xml ./
-rm -rf temp
-chmod 0644 com.android.systemui.xml
-cd -
 
 tg_sendText "Lunching"
 # Normal build steps
 . build/envsetup.sh
-lunch corvus_a10-userdebug
+export BUILD_USERNAME=hiratazx
+riseup beyond0lte userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -74,14 +42,16 @@ ccache -z
 
 tg_sendText "Starting Compilation.."
 
-mka bacon -j10 | tee build.txt
+rise b | tee build-ota.txt
+rise fb | tee build-fastboot.txt
 
 tg_sendText "Build completed! Uploading rom to gdrive"
-rclone copy out/target/product/a10/*Unofficial* aosp:final -P || rclone copy out/target/product/a10/*Alpha*.zip aosp:final -P
+rclone copy out/target/product/beyond0lte/*fastboot.zip hiratazx:ItzKaguya -P || rclone copy out/target/product/beyond0lte/*ota.zip hiratazx:ItzKaguya -P
 
-(ccache -s && echo " " && free -h && echo " " && df -h && echo " " && ls -a out/target/product/a10/) | tee final_monitor.txt
+(ccache -s && echo " " && free -h && echo " " && df -h && echo " " && ls -a out/target/product/beyond0lte/) | tee final_monitor.txt
 tg_sendFile "final_monitor.txt"
-tg_sendFile "build.txt"
+tg_sendFile "build-fastboot.txt"
+tg_sendFile "build-ota.txt"
 
 #tg_sendText "Uploading new ccache to gdrive"
 #cd /tmp
